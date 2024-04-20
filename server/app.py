d74268ae843be9f1a1b7d31213ec1c3ba2be7cff
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2024-04-20 15:29:29 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -47,6 +47,28 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self,id):
+        json = request.get_json()
+        plant = Plant.query.filter_by(id = id).first()
+
+        for attr in json:
+            setattr(plant, attr, json[attr])
+
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response(plant.to_dict(),200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        response_dict = {"message" : ""}
+
+        return make_response(response_dict,204)
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
